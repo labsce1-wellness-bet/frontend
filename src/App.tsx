2effe3cd05f76a1e46008b1a2bb0bae69365f809
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-05-31 09:23:00 -0700

    change tab name

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "views/home/Home";
 
+document.title = 'Wellness Bet';
+
 const App: React.FC = () => {
   return (
     <div className="App">
