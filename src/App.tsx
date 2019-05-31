2a59b23007f77e2f352a07f1a16aad7e2d09e947
Simon Davis <simonfreelance1@gmail.com>
2019-05-30 23:14:29 -0400

    Added Route for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Home from "views/home/Home";
 
 const App: React.FC = () => {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Route exact path="/" component={Home} />
+    </div>
+  );
 };
 
 export default App;
