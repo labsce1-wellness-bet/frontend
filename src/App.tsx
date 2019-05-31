175cbf43c535320035c764a6f968f1b2308f517c
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-05-31 10:02:08 -0700

    change tab name

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "views/home/Home";
 
+document.title = 'Wellness Bet';
+
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <Route exact path="/" component={Home} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Route exact path="/" component={Home} />
+        </div>
+    );
 };
 
 export default App;
