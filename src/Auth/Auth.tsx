a3a60c8d3c59e41cc3c707ba4e7d9875c60ea401
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-06-05 08:37:22 -0700

    add netlify frontend

--- src/Auth/Auth.tsx
+++ src/Auth/Auth.tsx
@@ -4,7 +4,7 @@ export default class Auth {
     auth0 = new auth0.WebAuth({
         domain: 'akshay-gadkari.auth0.com',
         clientID: 'fX2Ov3PrG67snp7CsUrUFcFE8RN5aglD',
-        redirectUri: 'https://wellness-bet-frontend.herokuapp.com/callback',
+        redirectUri: 'https://wellness-bet.netlify.com/callback',
         responseType: 'token id_token',
         scope: 'openid'
     });
