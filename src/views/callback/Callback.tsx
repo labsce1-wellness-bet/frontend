d2067e7831358c3dd707ceb57369a091e218d767
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-06-05 12:15:13 -0700

    added netlify callback page

--- src/views/callback/Callback.tsx
+++ src/views/callback/Callback.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export interface HomeProps { }
 
-const fetchUserData = () => {
-    axios
-	.get('https://wellness-bet-backend.herokuapp.com/users/1')
-	.then(res => {
-	    const user = res.data;
-	    this.set
-	})
-	.catch(err => console.log(err));
-}
+/* const fetchUserData = () => {
+ *     axios
+ * 	.get('https://wellness-bet-backend.herokuapp.com/users/1')
+ * 	.then(res => {
+ * 	    const user = res.data;
+ * 	    this.set
+ * 	})
+ * 	.catch(err => console.log(err));
+ * }*/
 
 const Callback: React.SFC<HomeProps> = () => {
     /* constructor(props) {
@@ -33,8 +33,8 @@ const Callback: React.SFC<HomeProps> = () => {
     return <div>
         Home
         <h1>Callback page, you're logged in.</h1>
-        <p>{users.id}</p>
     </div>;
 };
 
 export default Callback;
+
