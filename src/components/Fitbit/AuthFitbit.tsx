a40e27791b27ebe32d0269679fcb21022e36ce6a
ckirkwood94 <ckirkwood94@gmail.com>
2019-06-07 10:06:59 -0400

    Add component that redirects to fitbits auth page

--- src/components/Fitbit/AuthFitbit.tsx
+++ src/components/Fitbit/AuthFitbit.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-export interface AuthFitbitProps {}
-const AuthFitbit: React.SFC<AuthFitbitProps> = () => {
-  return <button />;
+export interface AuthFitbitProps {
+  userId: number;
+}
+
+const AuthFitbit: React.SFC<AuthFitbitProps> = props => {
+  console.log("AuthFitbit", props);
+  const state = `userId=${props.userId}`;
+  const url: string = "http://localhost:3000/fitbit/connecting";
+  const encodedUrl: string = encodeURI(url);
+
+  return (
+    <a
+      href={`https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22DR4B&redirect_uri=${encodedUrl}&scope=sleep&state=${state}`}>
+      Connect Fitbit
+    </a>
+  );
 };
 
 export default AuthFitbit;
