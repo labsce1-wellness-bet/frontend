291da3e75587ea734695692182c0b6f3880ec18a
Simon Davis <simon_freelance@hotmail.com>
2019-06-10 19:53:02 -0400

    Created <MaterialSimpleModal />

---
+++ src/components/MaterialSimpleModal/MaterialSimpleModal.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(4),
+    outline: "none",
+  },
+}));
+export interface Props {
+  setIsOpen: Function;
+  isOpen: boolean;
+  children: React.ReactElement;
+  [key: string]: any;
+}
+
+const MaterialSimpleModal: React.SFC<Props> = props => {
+  const [modalStyle] = useState(getModalStyle);
+  const classes = useStyles();
+
+  return (
+    <Modal
+      open={props.isOpen}
+      onClose={() => props.setIsOpen(false)}
+      {...props}>
+      <div style={modalStyle} className={classes.paper}>
+        {props.children}
+      </div>
+    </Modal>
+  );
+};
+
+export { MaterialSimpleModal };
