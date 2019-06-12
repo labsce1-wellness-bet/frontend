85f4ac83772deb74082b87746bde96e15bd4af30
Simon Davis <simon_freelance@hotmail.com>
2019-06-11 20:35:29 -0400

    Basic dashboard page added with start section

---
+++ src/views/dashboard/Dashboard.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import { DashboardStart } from "./sections/dashboard-start/DashboardStart";
+import { DashboardWrapper } from "./DashboardStyles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  SwipeableDrawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+} from "@material-ui/core";
+import { Settings, Group } from "@material-ui/icons";
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    top: "auto",
+    bottom: 0,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  fullList: {
+    width: "auto",
+  },
+}));
+interface Props {}
+const Dashboard: React.SFC<Props> = () => {
+  const classes = useStyles();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  return (
+    <DashboardWrapper>
+      <Route path="/dashboard/start" component={DashboardStart} />
+
+      {/* Every page for dashboard has the below components */}
+      <SwipeableDrawer
+        anchor="left"
+        open={isDrawerOpen}
+        onOpen={setIsDrawerOpen.bind(null, true)}
+        onClose={setIsDrawerOpen.bind(null, false)}>
+        <div className={classes.fullList}>
+          <header>
+            {/* TODO: Bring in data for name and email of user */}
+            <h2>Jake Johnson</h2>
+            <p>jake.johnson@email.com</p>
+          </header>
+          <Divider />
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <Group />
+              </ListItemIcon>
+              <ListItemText>Groups</ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText>Settings</ListItemText>
+            </ListItem>
+          </List>
+        </div>
+      </SwipeableDrawer>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+            onClick={setIsDrawerOpen.bind(null, true)}>
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </DashboardWrapper>
+  );
+};
+
+export default Dashboard;
---
+++ src/views/dashboard/sections/dashboard-start/DashboardStart.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { DashboardStartWrapper, Content } from "./DashboardStartStyles";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+interface Props {}
+
+const DashboardStart: React.SFC<Props> = () => {
+  return (
+    <DashboardStartWrapper>
+      <Content>
+        <Link to="#">
+          <Button
+            variant="outlined"
+            color="primary"
+            className="dashboard-start__btn">
+            Create New Group
+          </Button>
+        </Link>
+        <Link to="#">
+          <Button
+            variant="outlined"
+            color="primary"
+            className="dashboard-start__btn">
+            Join With Code
+          </Button>
+        </Link>
+      </Content>
+    </DashboardStartWrapper>
+  );
+};
+
+export { DashboardStart };
