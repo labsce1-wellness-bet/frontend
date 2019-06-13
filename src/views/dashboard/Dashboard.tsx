fa542526d2cf10077aadf6642df2fc1b185d8e53
Simon Davis <simon_freelance@hotmail.com>
2019-06-12 20:27:31 -0400

    Added plus button on app bar that opens a bottom drawer

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -10,6 +10,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import AddIcon from "@material-ui/icons/Add";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   SwipeableDrawer,
@@ -26,6 +27,10 @@ const useStyles = makeStyles(theme => ({
     top: "auto",
     bottom: 0,
   },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
@@ -36,7 +41,8 @@ const useStyles = makeStyles(theme => ({
 interface Props {}
 const Dashboard: React.SFC<Props> = () => {
   const classes = useStyles();
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isMenuDrawerOpen, setIsMenuDrawerOpen] = useState(false);
+  const [isPlusDrawerOpen, setIsPlusDrawerOpen] = useState(false);
   return (
     <DashboardWrapper>
       <Route exact path="/dashboard/start" component={DashboardStart} />
@@ -49,9 +55,9 @@ const Dashboard: React.SFC<Props> = () => {
       {/* Every page for dashboard has the below components */}
       <SwipeableDrawer
         anchor="left"
-        open={isDrawerOpen}
-        onOpen={setIsDrawerOpen.bind(null, true)}
-        onClose={setIsDrawerOpen.bind(null, false)}>
+        open={isMenuDrawerOpen}
+        onOpen={setIsMenuDrawerOpen.bind(null, true)}
+        onClose={setIsMenuDrawerOpen.bind(null, false)}>
         <div className={classes.fullList}>
           <header>
             {/* TODO: Bring in data for name and email of user */}
@@ -76,17 +82,51 @@ const Dashboard: React.SFC<Props> = () => {
         </div>
       </SwipeableDrawer>
       <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
+        <Toolbar className={classes.toolbar}>
           <IconButton
             edge="start"
             className={classes.menuButton}
             color="inherit"
-            aria-label="Menu"
-            onClick={setIsDrawerOpen.bind(null, true)}>
+            aria-label="open drawer"
+            onClick={setIsMenuDrawerOpen.bind(null, true)}>
             <MenuIcon />
+          </IconButton>
+          <IconButton
+            edge="end"
+            color="inherit"
+            onClick={setIsPlusDrawerOpen.bind(null, true)}>
+            <AddIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
+      <SwipeableDrawer
+        anchor="bottom"
+        open={isPlusDrawerOpen}
+        onOpen={setIsPlusDrawerOpen.bind(null, true)}
+        onClose={setIsPlusDrawerOpen.bind(null, false)}>
+        <div className={classes.fullList}>
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <AddIcon />
+              </ListItemIcon>
+              <ListItemText>Create New Group</ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <AddIcon />
+              </ListItemIcon>
+              <ListItemText>Join With Code</ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <AddIcon />
+              </ListItemIcon>
+              <ListItemText>Log Sleep</ListItemText>
+            </ListItem>
+          </List>
+        </div>
+      </SwipeableDrawer>
     </DashboardWrapper>
   );
 };
