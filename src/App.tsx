40375d1ecdfa71f3ca17359d1a0007b6e96e4c9f
ckirkwood94 <ckirkwood94@gmail.com>
2019-06-14 02:31:16 -0400

    add fitbit route

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Callback from "views/callback/Callback";
 import AuthPage from "views/auth/Auth";
+import Fitbit from "views/fitbit/Fitbit";
 import Auth from "./Auth/Auth"; //Auth0 code
 
 //run auth0
-const auth = new Auth();
-auth.login();
+// const auth = new Auth();
+// auth.login();
 
 //The name of the tab
 document.title = "Wellness Bet deploy";
 
-const App: React.FC = () => {
+const App: React.FC = props => {
   return (
     <div className="App">
       <Route exact path="/" component={AuthPage} />
       <Route exact path="/callback" component={Callback} />
+      <Route path="/fitbit" component={Fitbit} />
     </div>
   );
 };
