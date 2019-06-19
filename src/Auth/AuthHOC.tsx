8bbc4101e7e59395989691f1882b77eda196ae81
ckirkwood94 <ckirkwood94@gmail.com>
2019-06-19 11:27:43 -0400

    Started on AuthHOC

---
+++ src/Auth/AuthHOC.tsx
@@ -0,0 +1,20 @@
+// import React, { Component } from "react";
+// import { Redirect } from "react-router-dom";
+
+// interface Protected {
+//   children?: React.ReactNode;
+// }
+
+// const AuthHOC: React.FC = Protected => {
+//   class ProtectedChild extends Component {
+//     render() {
+//       if (this.props.authStatus === 200) {
+//         return <Protected {...this.props} />;
+//       } else {
+//         <Redirect to="/" />;
+//       }
+//     }
+//   }
+// };
+
+// export default AuthHOC;
