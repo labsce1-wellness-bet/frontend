d023aa47f85f15d9afd7ab437c4df231ec199cc5
ckirkwood94 <ckirkwood94@gmail.com>
2019-06-21 03:59:29 -0400

    Complete desktop view for new groups

--- src/views/dashboard/sections/dashboard-new-group/DashboardNewGroupStyles.tsx
+++ src/views/dashboard/sections/dashboard-new-group/DashboardNewGroupStyles.tsx
@@ -1,22 +1,55 @@
 import styled from "styled-components";
-import { Button } from "@material-ui/core";
+import { device } from "../../../../mediaQueries";
 
 const DashboardNewGroupWrapper = styled.div`
   display: flex;
   justify-content: center;
+
+  @media ${device.laptop} {
+    width: 1000px;
+    margin: 0 auto;
+    flex-wrap: wrap;
+  }
+
+  .button {
+    width: 50%;
+    margin: 20px auto 100px;
+
+    @media ${device.laptop} {
+      width: 200px;
+      margin: 30px auto 100px;
+    }
+  }
+  .title {
+    display: none;
+    @media ${device.laptop} {
+      display: block;
+      width: 100%;
+      margin-top: 100px;
+    }
+  }
 `;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   width: 90%;
-`;
 
-const CustomButton = styled(Button)`
-  width: 50%;
-  && {
-    margin: 20px auto 100px;
+  @media ${device.laptop} {
+    width: 80%;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    .input {
+      width: 45%;
+    }
+    .message-input {
+      width: 100%;
+      textarea {
+        height: 160px;
+      }
+    }
   }
 `;
 
-export { DashboardNewGroupWrapper, Form, CustomButton };
+export { DashboardNewGroupWrapper, Form };
