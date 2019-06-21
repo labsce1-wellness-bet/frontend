1686d20fa0cde4f74151ec10cb68acd8d3212b96
ckirkwood94 <ckirkwood94@gmail.com>
2019-06-21 12:26:43 -0400

    add another group

--- src/GlobalContext/UserContext.js
+++ src/GlobalContext/UserContext.js
@@ -14,7 +14,7 @@ const UserContext = React.createContext({
       adminName: "Caleb Kirkwood",
       betAmount: 50,
       startDate: moment()
-        .add(3, "days")
+        .add(5, "days")
         .format("M/DD/YYYY"),
       endDate: moment()
         .add(12, "days")
@@ -54,6 +54,52 @@ const UserContext = React.createContext({
           progress: 31,
           paid: true,
           last_five: [8, 15, 22, 26, 31],
+        },
+      ],
+      admin: 423,
+      adminName: "Jonathan Holloway",
+      betAmount: 25,
+      startDate: moment()
+        .subtract(5, "days")
+        .format("M/DD/YYYY"),
+      endDate: moment()
+        .add(22, "days")
+        .format("M/DD/YYYY"),
+      potTotal: 100,
+      message:
+        "It's time for us to start getting the sleep we need. If you're like me, you're feeling sluggish and slow all day. Let's compete and get the sleep we need! Call me mama bear because I'm going to hibernate and win this thing!",
+    },
+    {
+      groupId: 3,
+      groupName: "Sleepers",
+      members: [
+        {
+          fname: "Caleb",
+          lname: "Kirkwood",
+          progress: 0,
+          paid: true,
+          last_five: [0, 0, 0, 0, 0],
+        },
+        {
+          fname: "Jonathan",
+          lname: "Holloway",
+          progress: 0,
+          paid: true,
+          last_five: [0, 0, 0, 0, 0],
+        },
+        {
+          fname: "John",
+          lname: "Schmidt",
+          progress: 0,
+          paid: true,
+          last_five: [0, 0, 0, 0, 0],
+        },
+        {
+          fname: "Mary",
+          lname: "Smith",
+          progress: 0,
+          paid: true,
+          last_five: [0, 0, 0, 0, 0],
         },
       ],
       admin: 423,
