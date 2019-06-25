e90aa7e00f71eb36d5c4a4e24f671ac3641670f3
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-06-24 17:52:37 -0700

    get user info

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -6,7 +6,7 @@ export default class Auth {
 	clientID: "fX2Ov3PrG67snp7CsUrUFcFE8RN5aglD",
 	redirectUri: process.env.REACT_APP_FRONTEND_REDIRECT,
 	responseType: "token id_token",
-	scope: "openid",
+	scope: "openid profile",
     });
     
     signup(signupParams, successCb, errorCb) {
@@ -59,15 +59,19 @@ export default class Auth {
 	    }
 	);
     }
-    getUserInfo() {
+    getUserInfo(successCb, errorCb) {
 	this.auth0.parseHash({ hash: window.location.hash }, (err, authResult) => {
 	    if (err) {
 		return console.log(err);
 	    }
 	    console.log("AUTH", authResult);
+	    window.localStorage.setItem("access_token", authResult.accessToken);
 	    this.auth0.client.userInfo(authResult.accessToken, (err, user) => {
 		if (err) {
 		    return console.log(err);
+		}
+		if (successCb) {
+		    successCb(user);
 		}
 		console.log("USER", user);
 		return user;
--- src/views/test/Test.tsx
+++ src/views/test/Test.tsx
@@ -1,95 +1,83 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
-  UploadImageWidget,
-  uploadWidgetReducer,
+    UploadImageWidget,
+    uploadWidgetReducer,
 } from "components/UploadImageWidget/index";
 
 import { UploadAvatarPhoto } from "components/UploadAvatarPhoto/index";
 import uploadToCloudinaryReducer from "lib/utils/uploadImageToCloudinary/uploadToCloudinaryReducer";
 import uploadImageToCloudinary from "lib/utils/uploadImageToCloudinary/uploadImageToCloudinary";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import Auth from "Auth/Auth.js";
-import AuthFitbit from "components/Fitbit/AuthFitbit";
 
 interface Props {}
-
-var request = require("request");
-
-var options = { method: 'POST',
-		url: 'https://akshay-gadkari.auth0.com/oauth/token',
-		headers: { 'content-type': 'application/x-www-form-urlencoded' },
-		form: 
-   { grant_type: 'authorization_code',
-     client_id: 'fX2Ov3PrG67snp7CsUrUFcFE8RN5aglD',
-     client_secret: process.env.YOUR_CLIENT_SECRET,
-     code: 'YOUR_AUTHORIZATION_CODE',
-     redirect_ui: process.env.CALLBACK_PAGE
-   }
-};
-
-
-request(options, function (error: object, response: object, body: object) {
-    //if (error) throw new Error(error);
-    console.log("body", body);
-});
-
-/* var options = { method: 'GET',
- * 		url: 'https://akshay-gadkari.auth0.com/api/v2/users',
- * 		qs: { q: 'name:"caleb kirkwood"', search_engine: 'v2' },
- * 		headers: { authorization: 'Bearer YOUR_MGMT_API_ACCESS_TOKEN' } };*/
-
-/* request(options, function (error: object, response: object, body: object) {
- *     console.log("body", body);
- * });*/
-
+interface UserInfo {
+    name: string;
+    nickname: string;
+}
 const Test: React.SFC<Props> = () => {
-  const [uwState, uwDispatch] = uploadWidgetReducer();
-  const [cloudState, cloudDispatch] = uploadToCloudinaryReducer();
-  const { base64ImageData } = uwState;
-  useEffect(() => {
-    const auth = new Auth();
-    console.log(auth.getUserInfo());
-  }, []);
+    const [uwState, uwDispatch] = uploadWidgetReducer();
+    const [cloudState, cloudDispatch] = uploadToCloudinaryReducer();
+    const { base64ImageData } = uwState;
+    const [state, setState] = useState({} as UserInfo);
+    useEffect(() => {
+	const auth = new Auth();
+	/* console.log("GETUSERINFO", auth.getUserInfo());*/
+	auth.getUserInfo(setState);
+    }, []);
 
-  return (
-    <div>
-      <UploadImageWidget
-        startingInstructions={"Drag and drop photo evidence of payment here."}
-        afterUploadInstructions={
-          "Drag and drop new photo to change current one."
-        }
-        state={uwState}
-        dispatch={uwDispatch}
-      />
-      <UploadAvatarPhoto state={""} dispatch={() => {}} />
-      <button
-        onClick={async () => {
-          const data = await uploadImageToCloudinary(
-            {
-              upload_preset: "users-receipts",
-              public_id: "receipt-from-user1234",
-            },
-            base64ImageData,
-            cloudDispatch,
-          );
-          console.log({ data });
-          console.log({ cloudState });
-        }}>
-        Upload Image
-      </button>
-      <button
-        onClick={async () => {
-          const data = await axios({
-            method: "get",
-            url: process.env.REACT_APP_BACKEND_PRODUCTION_URL + "api/user/1",
-          });
-          console.log(data);
-        }}>
-        Get user data
-      </button>
-      <AuthFitbit userId={1} />
-    </div>
-  );
+    return (
+	<div>
+            <div>
+		<h1>{state.name}</h1>
+	    </div>
+	    <UploadImageWidget
+	    startingInstructions={"Drag and drop photo evidence of payment here."}
+	    afterUploadInstructions={
+		"Drag and drop new photo to change current one."
+	    }
+	    state={uwState}
+	    dispatch={uwDispatch}
+	    />
+	    <UploadAvatarPhoto state={""} dispatch={() => {}} />
+	    <button
+		onClick={async () => {
+			const data = await uploadImageToCloudinary(
+			    {
+				upload_preset: "users-receipts",
+				public_id: "receipt-from-user1234",
+			    },
+			    base64ImageData,
+			    cloudDispatch,
+			);
+			console.log({ data });
+			console.log({ cloudState });
+		}}>
+		Upload Image
+	    </button>
+	    <button
+		onClick={async () => {
+			try {
+			    const { data }: AxiosResponse<any> = await axios({
+				headers: {
+				    Authorization: `Bearer ${window.localStorage.getItem(
+                  "access_token",
+                )}`,
+				},
+				method: "get",
+				url:
+                    process.env.REACT_APP_BACKEND_LOCAL_URL +
+                    "api/group/id/1/public/all/user-info",
+			    });
+			    console.log(data);
+			} catch (err) {
+			    console.log(err);
+			}
+		}}>
+		Get user data
+	    </button>
+	</div>
+    );
 };
 
 export default Test;
