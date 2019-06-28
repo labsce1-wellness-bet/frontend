ecdc91785760146f67038d2733510a5b57e9723a
ckirkwood94 <ckirkwood94@gmail.com>
2019-06-28 07:08:38 -0400

    Implement secure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import PrivateRoute from "components/PrivateRoute/PrivateRoute";
 import Callback from "views/callback/Callback";
@@ -6,23 +6,21 @@ import Test from "views/test/Test";
 import AuthPage from "views/auth/Auth";
 import Fitbit from "views/fitbit/Fitbit";
 import Dashboard from "views/dashboard/Dashboard";
-import { GlobalContext } from "./GlobalContext/GlobalContext";
+import { GlobalContextProvider } from "./GlobalContext/GlobalContext";
 
 //The name of the tab
 document.title = "Wellness Bet deploy";
 
 const App: React.FC = props => {
-  const globalContext: { isAuthenticated: boolean } = useContext(GlobalContext);
-
   return (
     <div className="App">
-      <GlobalContext.Provider value={globalContext}>
+      <GlobalContextProvider>
         <Route exact path="/" component={AuthPage} />
-        <PrivateRoute exact path="/callback" component={Callback} />
+        <Route exact path="/callback" component={Callback} />
         <PrivateRoute path="/test" component={Test} />
         <PrivateRoute path="/dashboard" component={Dashboard} />
         <PrivateRoute path="/fitbit" component={Fitbit} />
-      </GlobalContext.Provider>
+      </GlobalContextProvider>
     </div>
   );
 };
--- src/views/callback/Callback.tsx
+++ src/views/callback/Callback.tsx
@@ -1,68 +1,30 @@
-import React from "react";
-// import React, { useState } from "react";
-/* import { VictoryPie } from "victory";*/
-import { VictoryChart } from "victory";
-import { VictoryGroup } from "victory";
-import { VictoryArea } from "victory";
+import React, { useState, useEffect, useContext, useReducer } from "react";
+import {
+  GlobalContext,
+  useGlobalContextValue,
+} from "GlobalContext/GlobalContext";
+import Auth from "Auth/Auth";
 
-export interface HomeProps { }
+export interface Props {
+  history: any;
+}
 
-/* 
- * function Chart() {
- *     const [todos, setTodos] = useState([
- *  	{
- *  	    text: 'Learn about React',
- *  	    isCompleted: false
- *  	},
- *  	{
- *  	    text: 'todo2',
- *  	    isCompleted: false
- *  	},
- *  	{
- *  	    text: 'Build app',
- *  	    isCompleted: false
- *  	}
- *     ]);*/
+const Callback: React.SFC<Props> = props => {
+  const [state, dispatch] = useGlobalContextValue();
 
+  useEffect(() => {
+    const auth0 = new Auth();
+    auth0.getUserInfo();
+    if (window.localStorage.access_token) {
+      dispatch({ type: "isAuth" });
+      console.log("useEffect", state.isAuthenticated);
 
-
-const Callback: React.SFC<HomeProps> = () => {
-    
-    return <div>
-	<VictoryChart width={400} height={400}>
-            <VictoryGroup
-		style={{
-		    data: { strokeWidth: 3, fillOpacity: 0.4 }
-		}}
-            >
-		<VictoryArea
-		    style={{
-			data: { fill: "cyan", stroke: "cyan" }
-		    }}
-		    data={[
-			{ x: 1, y: 2 },
-			{ x: 2, y: 3 },
-			{ x: 3, y: 5 },
-			{ x: 4, y: 4 },
-			{ x: 5, y: 7 }
-		    ]}
-		/>
-		<VictoryArea
-		    style={{
-			data: { fill: "magenta", stroke: "magenta" }
-		    }}
-		    data={[
-			{ x: 1, y: 3 },
-			{ x: 2, y: 2 },
-			{ x: 3, y: 6 },
-			{ x: 4, y: 2 },
-			{ x: 5, y: 6 }
-		    ]}
-		/>
-            </VictoryGroup>
-	</VictoryChart>
-    </div>;
+      props.history.push("/dashboard/start");
+    } else {
+      props.history.push("/");
+    }
+  }, []);
+  return <div></div>;
 };
 
 export default Callback;
-
