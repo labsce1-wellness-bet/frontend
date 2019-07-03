546c04dcb89c001e928174cf8017053e3ac94407
akshay-gadkari <34847501+akshay-gadkari@users.noreply.github.com>
2019-07-03 08:53:15 -0700

    Update Dashboard.tsx

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Route, Link } from "react-router-dom";
 
 import { DashboardStart } from "./sections/dashboard-start/DashboardStart";
