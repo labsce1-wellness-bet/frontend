6bff0f029fd09830bc86e5db2ecdec15d3e41582
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-07-04 12:16:58 -0700

    editing signup

--- src/views/auth/sections/SignupSection/SignupSection.tsx
+++ src/views/auth/sections/SignupSection/SignupSection.tsx
@@ -10,19 +10,7 @@ import {
 import RoundedInputBox from "components/RoundedInputBox/RoundedInputBox";
 import { Lock, Email } from "@material-ui/icons";
 import Fab from "@material-ui/core/Fab";
-
-// import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-//import Button from "@material-ui/core/Button";
-//import Snackbar from "@material-ui/core/Snackbar";
-//import IconButton from "@material-ui/core/IconButton";
-//import CloseIcon from "@material-ui/icons/Close";
 import Auth from "Auth/Auth.js";
-
-//import { toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// toast.configure({
-//     hideProgressBar: true,
-// });
 
 const SignupSection = () => {
   const [values, setValues] = useState({
