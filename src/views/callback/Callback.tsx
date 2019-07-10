878789726c9e50d6164f022d489e12af4f1ee166
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-07-10 12:53:37 -0700

    fix callbcak function

--- src/views/callback/Callback.tsx
+++ src/views/callback/Callback.tsx
@@ -1,6 +1,39 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+// import {
+//   //  GlobalContext,
+//   useGlobalContextValue,
+// } from "GlobalContext/GlobalContext";
+// import Auth from "Auth/Auth";
+// import { useUserContextValue } from "GlobalContext/_UserContext";
+
+// export interface Props {
+//   history: any;
+// }
+
+// const Callback: React.SFC<Props> = props => {
+//   const [dispatch] = useGlobalContextValue();
+//   const [userDispatch] = useUserContextValue();
+
+//   const handleGetUserInfo = (user: any) => {
+//     userDispatch({ type: "setUserInfo", user });
+//   };
+
+//   useEffect(() => {
+//     const auth0 = new Auth();
+//     dispatch({ type: "isAuth" });
+//     if (state.isAuthenticated) {
+//       auth0.getUserInfo(handleGetUserInfo);
+//       props.history.push("/dashboard/start");
+//     }
+//   }, [state.isAuthenticated]);
+//   return <div></div>;
+// };
+
+// export default Callback;
+
+import React, { useState, useEffect, useContext, useReducer } from "react";
 import {
-  //  GlobalContext,
+  GlobalContext,
   useGlobalContextValue,
 } from "GlobalContext/GlobalContext";
 import Auth from "Auth/Auth";
@@ -11,8 +44,8 @@ export interface Props {
 }
 
 const Callback: React.SFC<Props> = props => {
-  const [dispatch] = useGlobalContextValue();
-  const [userDispatch] = useUserContextValue();
+  const [state, dispatch] = useGlobalContextValue();
+  const [userState, userDispatch] = useUserContextValue();
 
   const handleGetUserInfo = (user: any) => {
     userDispatch({ type: "setUserInfo", user });
